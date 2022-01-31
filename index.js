3a014336b54879d05d0fa16d7741c306e2bf5a55
LwtCompany <hunter982019@gmail.com>
2022-01-31 20:56:39 +0500

    foo

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const keys = require('./config/keys');
 require('./models/User');
 require('./models/Blog');
 require('./services/passport');
-//dgg
+
 mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI, { useMongoClient: true });
 
