aedac8837a237730a3f9a914f8015a769e21be78
LwtCompany <hunter982019@gmail.com>
2022-01-31 21:03:30 +0500

    progHunt settingvbs

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const keys = require('./config/keys');
 require('./models/User');
 require('./models/Blog');
 require('./services/passport');
-
+//ergrg
 mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI, { useMongoClient: true });
 
