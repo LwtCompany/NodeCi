70423ca42ad09aacf07ffcf46736d6d5bd144e8e
LwtCompany <hunter982019@gmail.com>
2022-01-31 20:53:13 +0500

    progHunt settings

---
+++ test/header.test.js
@@ -0,0 +1,36 @@
+const pupperteer = require('puppeteer');
+
+const Page = require('./helpers/page');
+
+let page;
+
+beforeEach(async() => {
+    page = await Page.build();
+    await page.goto('http://localhost:3000');
+})
+
+afterEach(async()=> {
+    await page.close();
+})
+
+test("the header is Blogster", async () => {
+    await page.login();
+    const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+    expect(text).toEqual('Blogster');
+});
+
+test('Clicking login starts oauth flow', async() => {
+    await page.click('.right a');
+    const url = await page.url();
+
+    expect(url).toMatch(/accounts\.google\.com/);
+})
+
+
+test('when signed in, show logout button', async() => {
+    
+    await page.login();
+    const text  = await page.getContentsOf('a[href="/auth/logout"]');
+
+    expect(text).toEqual('Logout');
+});
